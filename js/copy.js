8e9728af4afd336e5c488bbf6f049b619e490a51
Msretail100 <msretail100@gmail.com>
2025-09-14 23:00:48 -0600

    solo agregue comentarios

--- js/copy.js
+++ js/copy.js
@@ -1,8 +1,11 @@
+//es para copiar la contraseña al portapapeles
+
 let passwordText = document.querySelector(".result").innerText;
 function copiar() {
     let passwordText = document.querySelector(".result").innerText;
     let password = passwordText.replace("Tu resultado: ", "");
     navigator.clipboard.writeText(password).then(function() {
+        //confirmación visual
         alert("Contraseña copiada al portapapeles: " + password);
     }, function(err) {
         console.error("Error al copiar la contraseña: ", err);
